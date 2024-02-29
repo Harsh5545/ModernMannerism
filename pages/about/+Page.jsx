d75469524a61315c7cfb60c5c5b2700db901c805
harshad <Harshadkajale333@gmail.com>
2024-02-29 16:51:54 +0530

    first commit

---
+++ pages/about/+Page.jsx
@@ -0,0 +1,16 @@
+import './code.css'
+
+export { Page }
+
+
+export const title = 'SpaceX | Rocket Starship'
+
+function Page() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Example of using Vike.</p>
+    </>
+  )
+}
+
