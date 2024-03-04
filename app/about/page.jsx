52ff5490e7af611e17f308faa0af66ab6f24a13f
harshad <Harshadkajale333@gmail.com>
2024-03-04 22:00:07 +0530

    all new Commit

--- app/about/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>About Us 🔼🔼</div>
-  )
-}
-
-export default page
--- app/pricing/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>Price </div>
-  )
-}
-
-export default page
