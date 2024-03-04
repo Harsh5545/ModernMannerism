9707c25153366aad7bbc41667c17d34dccbc4333
harshad <Harshadkajale333@gmail.com>
2024-03-04 22:05:07 +0530

    all new

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,18 +1,4 @@
 import {
-<<<<<<< HEAD:components/navbar.jsx
-  Navbar as NextUINavbar,
-  NavbarContent,
-  NavbarMenu,
-  NavbarMenuToggle,
-  NavbarBrand,
-  NavbarItem,
-  NavbarMenuItem,
-} from "@nextui-org/navbar";
-import { Button } from "@nextui-org/button";
-import { Kbd } from "@nextui-org/kbd";
-import { Link } from "@nextui-org/link";
-import { Input } from "@nextui-org/input";
-=======
 	Button,
 	Kbd,
 	Link,
@@ -25,7 +11,6 @@ import { Input } from "@nextui-org/input";
 	NavbarItem,
 	NavbarMenuItem,
 } from "@nextui-org/react";
->>>>>>> 39b5213 (all new Commit):components/navbar.tsx
 
 import { link as linkStyles } from "@nextui-org/theme";
 
@@ -35,145 +20,37 @@ import clsx from "clsx";
 
 import { ThemeSwitch } from "@/components/theme-switch";
 import {
-  TwitterIcon,
-  GithubIcon,
-  DiscordIcon,
-  HeartFilledIcon,
-  SearchIcon,
+	TwitterIcon,
+	GithubIcon,
+	DiscordIcon,
+	HeartFilledIcon,
+	SearchIcon,
 } from "@/components/icons";
 
 import { Logo } from "@/components/icons";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-export const Navbar = async () => {
-  const session = await getServerSession(authOptions);
-  console.log(session);
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
+export const Navbar = () => {
+	const searchInput = (
+		<Input
+			aria-label="Search"
+			classNames={{
+				inputWrapper: "bg-default-100",
+				input: "text-sm",
+			}}
+			endContent={
+				<Kbd className="hidden lg:inline-block" keys={["command"]}>
+					K
+				</Kbd>
+			}
+			labelPlacement="outside"
+			placeholder="Search..."
+			startContent={
+				<SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
+			}
+			type="search"
+		/>
+	);
 
-<<<<<<< HEAD:components/navbar.jsx
-  return (
-    <NextUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-        <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Logo />
-            <p className="font-bold text-inherit">ACME</p>
-          </NextLink>
-        </NavbarBrand>
-        <ul className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <NextLink
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium"
-                )}
-                color="foreground"
-                href={item.href}
-              >
-                {item.label}
-              </NextLink>
-            </NavbarItem>
-          ))}
-        </ul>
-      </NavbarContent>
-
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
-      >
-        <NavbarItem className="hidden sm:flex gap-2">
-          <Link isExternal href={siteConfig.links.twitter} aria-label="Twitter">
-            <TwitterIcon className="text-default-500" />
-          </Link>
-          <Link isExternal href={siteConfig.links.discord} aria-label="Discord">
-            <DiscordIcon className="text-default-500" />
-          </Link>
-          <Link isExternal href={siteConfig.links.github} aria-label="Github">
-            <GithubIcon className="text-default-500" />
-          </Link>
-          <ThemeSwitch />
-        </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-        <NavbarItem className="hidden md:flex">
-          <Button
-            isExternal
-            as={Link}
-            className="text-sm font-normal text-default-600 bg-default-100"
-            href={siteConfig.links.sponsor}
-            startContent={<HeartFilledIcon className="text-danger" />}
-            variant="flat"
-          >
-            Sponsor
-          </Button>
-        </NavbarItem>
-        {session && session?.user ? (
-          <>
-            <NavbarItem className="hidden lg:flex">
-              {session?.user.email}
-            </NavbarItem>
-            <Link isExternal variant="flat" href="/auth/signout">
-              Logout
-            </Link>
-          </>
-        ) : (
-          ""
-        )}
-      </NavbarContent>
-
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal href={siteConfig.links.github} aria-label="Github">
-          <GithubIcon className="text-default-500" />
-        </Link>
-        <ThemeSwitch />
-        <NavbarMenuToggle />
-      </NavbarContent>
-
-      <NavbarMenu>
-        {searchInput}
-        <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                    ? "danger"
-                    : "foreground"
-                }
-                href="#"
-                size="lg"
-              >
-                {item.label}
-              </Link>
-            </NavbarMenuItem>
-          ))}
-        </div>
-      </NavbarMenu>
-    </NextUINavbar>
-  );
-=======
 	return (
 		<NextUINavbar maxWidth="xl" position="sticky">
 			<NavbarContent className="basis-1/5 sm:basis-full" justify="start">
@@ -261,5 +138,4 @@ export const Navbar = async () => {
 			</NavbarMenu>
 		</NextUINavbar>
 	);
->>>>>>> 39b5213 (all new Commit):components/navbar.tsx
 };
