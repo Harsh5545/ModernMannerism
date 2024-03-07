b4535c8018ed9e7fa382b3fec5976353b39a78ac
Bhavishm009 <bsanjay@devstringx.com>
2024-03-08 00:28:37 +0530

    login signup added

---
+++ app/actions/users/signUp.js
@@ -0,0 +1,27 @@
+'use server';
+
+import prisma from '@/libs/db';
+import bcrypt from 'bcryptjs';
+
+export const signUp = async (email, password) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            email,
+        },
+    });
+
+    if (user) {
+        return 'User with that email already exists.';
+    }
+
+    let Newpassword = bcrypt.hashSync(password, 10);
+
+    await prisma.user.create({
+        data: {
+            email,
+            password: Newpassword
+        },
+    });
+
+    return "Successfully created new user!";
+};
---
+++ components/auth/SignUpForm.jsx
@@ -0,0 +1,32 @@
+'use client';
+
+import React, { useState } from 'react';
+import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image, Button} from "@nextui-org/react";
+import { signUp } from '@/app/actions/users/signUp';
+
+
+const SignUpForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = async () => {
+        setMessage("Signing up...");
+        const message = await signUp(email, password);
+        setMessage(message);
+    };
+
+    return (
+        <div className='flex flex-col gap-4 bg-gray-400 p-4'>
+            <input type='text' value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+
+            <Button onClick={handleSubmit}>Sign up</Button>
+
+            <p>{message}</p>
+        </div>
+    );
+};
+
+export default SignUpForm;
