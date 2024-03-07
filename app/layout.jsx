30dc3447a0a356c1033da8c3adf1de34b5268bc9
sanjay <Bhavishm009@gmail.com>
2024-03-08 02:34:52 +0530

    new meta

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,12 +13,12 @@ export const metadata = {
     },
     description:
         "Join Modern Mannerism today and embark on a journey of refinement and success. Whether you're looking to enhance your professional image or polish your dining finesse,we are here to guide you every step of the way",
-    twitter: {
-        card: "summary_large_image",
-        title: "Modern Mannerism",
-        description:
-            "Join Modern Mannerism today and embark on a journey of refinement and success. Whether you're looking to enhance your professional image or polish your dining finesse,we are here to guide you every step of the way",
-    },
+    // twitter: {
+    //     card: "summary_large_image",
+    //     title: "Modern Mannerism",
+    //     description:
+    //         "Join Modern Mannerism today and embark on a journey of refinement and success. Whether you're looking to enhance your professional image or polish your dining finesse,we are here to guide you every step of the way",
+    // },
 };
 
 export default function RootLayout({ children }) {
