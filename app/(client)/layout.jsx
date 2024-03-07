4daca70495b9dbe663fb5cef3040f05f845bd4ea
harshad <Harshadkajale333@gmail.com>
2024-03-07 19:46:39 +0530

    add navbar

--- app/(client)/layout.jsx
+++ app/(client)/layout.jsx
@@ -4,42 +4,37 @@ import "../globals.css";
 import Link from "next/link";
 import { Providers } from "../Provider";
 import ThemeSwitcher from "@/components/ThemeSwitcher";
-
+import Navbar from "@/components/client/Navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: {
-        default: "Modern Mannerism",
-        template: "%s - Modern Mannerism"
-    },
-    description: "Join Modern Mannerism today and embark on a journey of refinement and success. Whether you're looking to enhance your professional image or polish your dining finesse,we are here to guide you every step of the way",
-    twitter: {
-        card: "summary_large_image",
-        title: "Modern Mannerism",
-        description: "Join Modern Mannerism today and embark on a journey of refinement and success. Whether you're looking to enhance your professional image or polish your dining finesse,we are here to guide you every step of the way"
-    },
-
+  title: {
+    default: "Modern Mannerism",
+    template: "%s - Modern Mannerism",
+  },
+  description:
+    "Join Modern Mannerism today and embark on a journey of refinement and success. Whether you're looking to enhance your professional image or polish your dining finesse,we are here to guide you every step of the way",
+  twitter: {
+    card: "summary_large_image",
+    title: "Modern Mannerism",
+    description:
+      "Join Modern Mannerism today and embark on a journey of refinement and success. Whether you're looking to enhance your professional image or polish your dining finesse,we are here to guide you every step of the way",
+  },
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <body className={inter.className}>
-                <Providers>
-                    <header className="py-6">
-                        <nav className="w-[90%] mx-auto flex items-center justify-between">
-                            <ul>
-                                <li>
-                                    <Link href="/">Home</Link>
-                                </li>
-                            </ul>
-                            <ThemeSwitcher />
-                        </nav>
-                    </header>
-                    <main>{children}</main>
-                </Providers>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+       
+        <Providers>
+         
+          <Navbar />
+        <main>{children}</main>
+        <div className="h-[100vh]">harshad</div>
+        </Providers>
+      </body>
+    </html>
+  );
 }
--- components/ThemeSwitcher.jsx
+++ components/ThemeSwitcher.jsx
@@ -1,19 +1,25 @@
 "use client"
 
 import React, { useState } from 'react'
-import { Button } from '@nextui-org/react';
+import {  Switch } from '@nextui-org/react';
 import { useTheme } from 'next-themes';
+import { SunIcon } from './admin/Icons/SunIcon';
+import { MoonIcon } from './admin/Icons/MoonIcon';
 
 
 const ThemeSwitcher = () => {
     const [mounted, setMounted] = useState(false);
     const { theme, setTheme } = useTheme()
     return (
-        <div className='flex gap-4'>
-            <Button size='sm' variant='flat' onClick={() => setTheme('light')}>Light</Button>
-            <Button size='sm' variant='flat' onClick={() => setTheme('dark')}>Dark</Button>
-            <Button size='sm' variant='flat' onClick={() => setTheme('modern')}>Modern</Button>
-        </div>
+       <Switch  onClick={() => setTheme(theme === "dark" ? "light" : theme === "light" ? "dark" : "light")}
+            defaultSelected
+            size="sm"
+            color="secondary"
+            
+            startContent={<SunIcon />}
+            endContent={<MoonIcon />}
+        >
+        </Switch>
     )
 }
 
