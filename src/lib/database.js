91a73c12eb2707066a0af1be931b9885a16204a4
sanjay <Bhavishm009@gmail.com>
2024-03-10 23:59:04 +0530

    new database settings

--- src/lib/database.js
+++ src/lib/database.js
@@ -2,9 +2,9 @@ import { createPool } from 'mysql2/promise';
 
 // Create a MySQL connection pool
 const pool = createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Harshad@5545',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
   database: 'modern-mannerism',
   connectionLimit: 10, 
 });
