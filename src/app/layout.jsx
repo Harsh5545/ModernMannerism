30ad363e43e912e037ec1d29ab18ddcafd660ce9
harshad <Harshadkajale333@gmail.com>
2024-03-12 21:52:26 +0530

    add your data

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,33 +2,37 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
-
+import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   manifest: "/manifest.json",
-  title: { 
-    default: "Modern Mannerism", 
-    template: "%s | Modern Mannerism", 
+  title: {
+    default: "Modern Mannerism",
+    template: "%s | Modern Mannerism",
   },
-  description: "Elevate your social grace with our specialized etiquette courses. From dining decorum to professional protocol, our classes provide practical insights and hands-on learning experiences to ensure you navigate any social setting with finesse.",
+  description:
+    "Elevate your social grace with our specialized etiquette courses. From dining decorum to professional protocol, our classes provide practical insights and hands-on learning experiences to ensure you navigate any social setting with finesse.",
 };
 
 export const viewport = {
-  themeColor: 'black',
-}
-
-
+  themeColor: "black",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="OHVM3XPAXIWBSVvyZS31w7VGFRXbjD05MblzRaM01WE"
+        />
+      </Head>
       <body className={inter.className}>
         <div className="container">
           <Navbar />
           {children}
           <Footer />
-
         </div>
       </body>
     </html>
