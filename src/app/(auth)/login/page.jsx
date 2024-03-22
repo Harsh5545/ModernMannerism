57a7b3b6020a870072af4a62a3687a1e62a1a1b2
Bhavish <Bhavishm009@gmail.com>
2024-03-22 15:21:13 +0530

    chages google auth added

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,20 +1,52 @@
-import { handleGithubLogin } from "@/lib/action";
+import { handleGithubLogin, handleGoogleLogin } from "@/lib/action";
 import { auth } from "@/lib/auth"
+import { Card, CardBody, CardFooter, CardHeader, Divider, Image } from "@nextui-org/react";
+import Link from "next/link";
+const LoginPage = async () => {
 
-const LoginPage = async () =>  {
+    const session = await auth();
+    console.log(session);
 
-  const session = await auth();
-  console.log(session);
 
- 
 
-  return (
-    <div>
-      <form action={handleGithubLogin}>
+    return (<>
 
-      <button>Login with Git Hub</button>
-      </form>
-    </div>
-  )
+
+        <Card className="max-w-[400px]  mx-auto">
+            <CardHeader className="flex gap-3">
+                <Image
+                    alt="nextui logo"
+                    height={40}
+                    radius="sm"
+                    src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+                    width={40}
+                />
+                <div className="flex flex-col">
+                    <p className="text-md">NextUI</p>
+                    <p className="text-small text-default-500">nextui.org</p>
+                </div>
+            </CardHeader>
+            <Divider />
+            <CardBody>
+                <form action={handleGithubLogin}>
+                    <button>Login with Git Hub</button>
+                </form>
+                <form action={handleGoogleLogin}>
+                    <button>Login with Google</button>
+                </form>
+            </CardBody>
+            <Divider />
+            <CardFooter>
+                <Link
+                    isExternal
+                    showAnchorIcon
+                    href="https://github.com/nextui-org/nextui"
+                >
+                    Visit source code on GitHub.
+                </Link>
+            </CardFooter>
+        </Card>
+    </>
+    )
 }
 export default LoginPage
--- src/lib/action.js
+++ src/lib/action.js
@@ -8,6 +8,11 @@ export const handleGithubLogin = async () => {
 
 }
 
+export const handleGoogleLogin = async () => {
+
+  await signIn("google");
+
+}
 
 export const handleLogout = async () => {
 
