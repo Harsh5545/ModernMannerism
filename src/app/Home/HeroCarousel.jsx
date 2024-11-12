f49e208c0119e6d6cd0fe54a1103889a2931a64a
Harsh <harshjaiswal28555@gmail.com>
2024-11-13 00:40:32 +0530

    carousel buttons removed

--- src/app/Home/HeroCarousel.jsx
+++ src/app/Home/HeroCarousel.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@nextui-org/react";
+import "../globals.css"
 import Image from "next/image";
 import React, { useRef, useState } from "react";
 import "swiper/css";
@@ -8,7 +9,6 @@ import "swiper/css/pagination";
 import "swiper/css/effect-fade";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Keyboard, Pagination, Navigation, Autoplay } from "swiper/modules";
-
 const HeroCarousel = () => {
   const swiperRef = useRef(null);
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -22,36 +22,36 @@ const HeroCarousel = () => {
       id: 1,
       image: "/assets/One-on-One Consulting.jpeg",
       text: "One-on-One Consulting",
-      description:"Help individuals refine their personal and professional image, develop strong communication skills, and boost their confidence. Our one-on-one sessions are customized to meet the unique needs of each client.",  
+      description: "Help individuals refine their personal and professional image, develop strong communication skills, and boost their confidence. Our one-on-one sessions are customized to meet the unique needs of each client.",
       button: "Sign Up Now",
     },
     {
       id: 2,
       image: "/assets/Corporate Workshops & Training.jpeg",
       text: "Corporate Workshops & Training",
-      description:"We offer customized training programs designed to improve workplace professionalism, communication, and team dynamics. These workshops are perfect for teams, managers, and executives who want to refine their professional image and etiquette.",      
-button: "Explore",
+      description: "We offer customized training programs designed to improve workplace professionalism, communication, and team dynamics. These workshops are perfect for teams, managers, and executives who want to refine their professional image and etiquette.",
+      button: "Explore",
     },
     {
       id: 3,
       image: "/assets/Young Adult Grooming Programme.jpeg",
       text: "Young Adult Grooming Programmee",
-       description:"Prepare teens and young adults for success by teaching them essential grooming, communication, and etiquette skills. This program is ideal for young adults entering the workforce, preparing for interviews, or simply wanting to build confidence in their daily lives.",      
- button: "Start",
+      description: "Prepare teens and young adults for success by teaching them essential grooming, communication, and etiquette skills. This program is ideal for young adults entering the workforce, preparing for interviews, or simply wanting to build confidence in their daily lives.",
+      button: "Start",
     },
-{
+    {
       id: 4,
       image: "/assets/Children's Etiquette Programme.jpeg",
       text: "Children's Etiquette Programmee",
-       description:"Help children learn good manners, respect, and social skills in a fun and engaging way. This program focuses on building the foundation for positive behavior and respectful interactions, preparing children for both social and school environments.",      
- button: "Start",
+      description: "Help children learn good manners, respect, and social skills in a fun and engaging way. This program focuses on building the foundation for positive behavior and respectful interactions, preparing children for both social and school environments.",
+      button: "Start",
     },
-{
+    {
       id: 5,
       image: "/assets/Online Courses & Workshops.jpeg",
       text: "Online Courses & Webinars",
-       description:"Our online offerings allow you to learn at your own pace or join live sessions to enhance your skills in image, etiquette, and communication.",      
- button: "Start",
+      description: "Our online offerings allow you to learn at your own pace or join live sessions to enhance your skills in image, etiquette, and communication.",
+      button: "Start",
     },
   ];
 
@@ -82,7 +82,7 @@ button: "Explore",
           background-color: #c70ba8; /* Customize this color as needed */
         }
         .swiper-pagination-bullet-active {
-          background-color: #ff0000; /* Customize active dot color */
+          background-color: #ff0000 !important; /* Customize active dot color */
         }
       `}</style>
       <Swiper
@@ -99,7 +99,7 @@ button: "Explore",
         }}
         speed={100}
         loop={true}
-        navigation={true}
+        navigation={false}
         modules={[Autoplay, Keyboard, Pagination, Navigation]}
         className="mySwiper"
         ref={swiperRef}
@@ -117,7 +117,7 @@ button: "Explore",
               <div className="absolute flex gap-3 md:gap-8 flex-col text-center items-center justify-center h-full z-10 p-8">
                 <h1 className={styles.text}>{slide.text}</h1>
                 <p className="md:w-[50%]  text-sm font-medium md:text-lg">{slide.description}</p>
-<Button className={styles.button}>{slide.button}</Button>
+                <Button className={styles.button}>{slide.button}</Button>
               </div>
             </div>
           </SwiperSlide>
